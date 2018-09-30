ad09a058383c04eb3f0d447d389bd098b396ec35
Mauricio Estrada <maucodev@hotmail.com>
2018-09-30 11:49:22 -0500

    A Basic Validation Implementation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,11 +3,12 @@ import Input from './common/input';
 
 class LoginForm extends Component {
     state = {
-        account: { username: '', password: '' }
+        account: { username: '', password: '' },
+        errors: { }
     }
 
     render() { 
-        const { account } = this.state;
+        const { account, errors } = this.state;
 
         return (
             <div>
@@ -18,11 +19,13 @@ class LoginForm extends Component {
                         name="username"
                         label="Username"
                         value={account.username}
+                        error={errors.username}
                         onChange={this.handleChange} />
                     <Input
                         name="password"
                         label="Password"
                         value={account.password}
+                        error={errors.password}
                         onChange={this.handleChange} />
                     <button className="btn btn-primary">Login</button>
                 </form>
@@ -32,14 +35,64 @@ class LoginForm extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
+        
+        const errors = this.validate();
+
+        this.setState(
+            {errors: errors || {}}
+        )
+
+        if (!errors) {
+            console.log('Yes');
+        }
     };
 
     handleChange = ({ currentTarget: input }) => {
+        const errors = {...this.state.errors};
         const account = {...this.state.account};
+        const errorMessage = this.validateProperty(input);
+
+        if (errorMessage) {
+            errors[input.name] = errorMessage;
+        } else {
+            delete errors[input.name];
+        }
+        
         account[input.name] = input.value;
+        
         this.setState({
-            account
+            account,
+            errors
         });
+    }
+
+    validate = () => {
+        const errors = {};
+        const { account } = this.state;
+
+        if (account.username.trim() === '') {
+            errors.username = 'Username is required.'
+        }
+
+        if (account.password.trim() === '') {
+            errors.password = 'Password is required.'
+        }
+
+        return Object.keys(errors).length === 0 ? null : errors;
+    };
+
+    validateProperty = ({ name, value }) => {
+        if (name === 'username') {
+            if (value.trim() === '') {
+                return 'Username is required';
+            }
+        }
+
+        if (name === 'password') {
+            if (value.trim() === '') {
+                return 'Password is required';
+            }
+        }
     }
 }
  
