b7abe9ba6bb889adb02fc16cfdf16b62c009c71f
Mauricio Estrada <maucodev@hotmail.com>
2018-09-29 20:04:49 -0500

    Handling Multiple Inputs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+    state = {
+        account: { username: '', password: '' }
+    }
+
     render() { 
+        const { account } = this.state;
+
         return (
             <div>
                 <h1>Login</h1>
@@ -9,11 +15,24 @@ class LoginForm extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input id="username" type="text" className="form-control"/>
+                        <input 
+                            value={account.username} 
+                            onChange={this.handleChange}
+                            id="username" 
+                            name="username"
+                            type="text" 
+                            className="form-control" 
+                            autoFocus />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input id="password" type="text" className="form-control"/>
+                        <input 
+                            value={account.password} 
+                            onChange={this.handleChange}
+                            id="password" 
+                            name="password"
+                            type="text" 
+                            className="form-control" />
                     </div>
                     <button className="btn btn-primary">Login</button>
                 </form>
@@ -24,6 +43,14 @@ class LoginForm extends Component {
     handleSubmit = e => {
         e.preventDefault();
     };
+
+    handleChange = ({ currentTarget: input }) => {
+        const account = {...this.state.account};
+        account[input.name] = input.value;
+        this.setState({
+            account
+        });
+    }
 }
  
 export default LoginForm;
