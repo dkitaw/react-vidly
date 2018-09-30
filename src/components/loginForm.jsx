cc2b24d62f61b732c4def1f1d73b5eacb261d373
Mauricio Estrada <maucodev@hotmail.com>
2018-09-30 12:39:02 -0500

    Disabling the Submit Button

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -33,7 +33,9 @@ class LoginForm extends Component {
                         value={account.password}
                         error={errors.password}
                         onChange={this.handleChange} />
-                    <button className="btn btn-primary">Login</button>
+                    <button 
+                        disabled={this.validate()}
+                        className="btn btn-primary">Login</button>
                 </form>
             </div>
         );
